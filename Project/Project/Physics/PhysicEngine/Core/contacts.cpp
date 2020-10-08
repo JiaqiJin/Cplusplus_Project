5ceba64d14b844ef97fc09963dd0bf78316e126a
JiaqiJin96 <fdsam@hotmail.com>
2020-10-08 12:31:34 +0200

    contact basis

--- Project/Project/Physics/PhysicEngine/Core/contacts.cpp
+++ Project/Project/Physics/PhysicEngine/Core/contacts.cpp
@@ -10,3 +10,50 @@ void Contact::setBodyData(RigidBody* one, RigidBody* two,
     Contact::friction = friction;
     Contact::restitution = restitution;
 }
+
+/*
+This is stored as a 3x3 matrix, where each vector is a column.
+The x direction is generated from the contact normal, and the y and z directions 
+are set so they are at right angles to it.
+*/
+void Contact::calculateContactBasis()
+{
+    Vector3 contactTangent[2];
+
+    //Check whether the Z - axis is nearer to the X or Y axis
+    if (real_abs(contactNormal.x) > real_abs(contactNormal.y))
+    {
+        //Scaling dactor ro ensure the result are normalised
+        const real s = (real)1.0f / real_sqrt(contactNormal.z * contactNormal.z +
+            contactNormal.x * contactNormal.x);
+
+        // The new X-axis is at right angles to the world Y-axis
+        contactTangent[0].x = contactNormal.z * s;
+        contactTangent[0].y = 0;
+        contactTangent[0].z = -contactNormal.x * s;
+
+        // The new Y-axis is at right angles to the new X- and Z- axes
+        contactTangent[1].x = contactNormal.y * contactTangent[0].x;
+        contactTangent[1].y = contactNormal.z * contactTangent[0].x - contactNormal.x * contactTangent[0].z;
+        contactTangent[1].z = -contactNormal.y * contactTangent[0].x;
+    }
+    else
+    {
+        // Scaling factor to ensure the results are normalised
+        const real s = (real)1.0 / real_sqrt(contactNormal.z * contactNormal.z +
+            contactNormal.y * contactNormal.y);
+        // The new X-axis is at right angles to the world X-axis
+        contactTangent[0].x = 0;
+        contactTangent[0].y = -contactNormal.z * s;
+        contactTangent[0].z = contactNormal.y * s;
+
+        // The new Y-axis is at right angles to the new X- and Z- axes
+        contactTangent[1].x = contactNormal.y * contactTangent[0].z -
+            contactNormal.z * contactTangent[0].y;
+        contactTangent[1].y = -contactNormal.x * contactTangent[0].z;
+        contactTangent[1].z = contactNormal.x * contactTangent[0].y;
+    }
+
+    // Make a matrix from the three vectors.
+    contactToWorld.setComponents(contactNormal, contactTangent[0], contactTangent[1]);
+}
--- Project/Project/Physics/PhysicEngine/Core/contacts.h
+++ Project/Project/Physics/PhysicEngine/Core/contacts.h
@@ -1,6 +1,6 @@
-#ifndef KAWAII_CONTACTS_H
+﻿#ifndef KAWAII_CONTACTS_H
 #define KAWAII_CONTACTS_H
-
+//336
 /*
 The contact resolution system.
 CONTACT DATA : 
@@ -8,10 +8,10 @@ CONTACT DATA :
 -The collision normal : Direction which an impact impulse will be feel between 2 obj
 -The penetration depth : This is the amount that the two objects are interpenetrating.
 Contact type :																							^
-POINT�FACE CONTACTS : The contact normal is given by the normal of the surface at the point of contact. |
-EDGE�EDGE CONTACTS : The contact normal is at right angles to the tangents of both edges.
-EDGE�FACE CONTACTS : Only used with curved surfaces.The contact normal is given by the normal of the face, as before.
-FACE�FACE CONTACTS : when a curved surface comes in contact with another face.The contact normal is given by the normal of the first face.
+POINT–FACE CONTACTS : The contact normal is given by the normal of the surface at the point of contact. |
+EDGE–EDGE CONTACTS : The contact normal is at right angles to the tangents of both edges.
+EDGE–FACE CONTACTS : Only used with curved surfaces.The contact normal is given by the normal of the face, as before.
+FACE–FACE CONTACTS : when a curved surface comes in contact with another face.The contact normal is given by the normal of the first face.
 
 algorithms :
 PRIMITIVE COLLISION ALGORITHMS
@@ -21,6 +21,29 @@ PRIMITIVE COLLISION ALGORITHMS
 
 namespace Kawaii
 {
+	/*
+	IMPULSES AND IMPULSIVE TORQUES
+	the collision will generate a linear change in velocity (the impulse) and an angular change in velocity.
+	An instanttaneous angular change in velicity = inmpulse torque. τ = I ¨O, for toruqe u = I`O.
+	u = impulse torque, I is the inertia tensor, Δ`O = I^-1 * u.
+
+	COLLISION IMPULSES
+	To resolver the relative motion of two objs we need to calculate 4 impulse 
+	-linear and angular impulses for each object. the impulse and impulsive torque for the single object.
+	To calculate the impulse and impulsive force on each object :
+	- Work in a set of coordinates that are relative to the contact. Create the transform matrix to convert inyo
+	  and out of this new set of coordinates.
+	-Wrok out the chnage in velocity of the contact point on each obj per unit impulse.
+	- We will know the velocity change we want to see, invert the result of th last stage to find the impulse needed.
+	-Wokr out what the separating velocity at the contact point sould be.
+	-From the desired change in velocity we can calculate the impulse that must be generated.
+	 split the impulse into its linear and angular components.
+
+	Change coordinates :
+	We interestng the movement of the collision point at this stage.
+	First step of converting to contact coordinates is to work out the direction of each axis.
+
+	*/
 	class Contact
 	{
 	public:
@@ -37,6 +60,17 @@ namespace Kawaii
 
 		/**/
 		void setBodyData(RigidBody* one, RigidBody* two, real friction, real restitution);
+
+	protected:
+		/*
+		A transform matrix that converts the coordinates in the contact frame of reference to world coordinates.
+		The columns of this matrix form an orthogonal set of vector.
+		*/
+		Matrix3 contactToWorld;
+
+	protected:
+		/*Calculate the normal basis for the contact point, based on the primary friction direction.*/
+		void calculateContactBasis();
 	};
 }
 
