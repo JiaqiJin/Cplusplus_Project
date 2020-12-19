777f11e4b081b368ba7e6e5b71db0fa656b885d5
kawakawaDe <fdsam@hotmail.com>
2020-12-19 18:14:29 +0000

    dx12

---
+++ Project/Project/DX12Practise/HelloWindow/D3D12HelloWindow.h
@@ -0,0 +1,67 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#pragma once
+
+#include "DXSample.h"
+
+// Note that while ComPtr is used to manage the lifetime of resources on the CPU,
+// it has no understanding of the lifetime of resources on the GPU. Apps must account
+// for the GPU lifetime of resources to avoid destroying objects that may still be
+// referenced by the GPU.
+// An example of this can be found in the class method: OnDestroy().
+using Microsoft::WRL::ComPtr;
+
+class D3D12HelloWindow : public DXSample
+{
+public:
+    D3D12HelloWindow(UINT width, UINT height, std::wstring name);
+
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnDestroy();
+
+private:
+    static const UINT FrameCount = 2;
+
+    ComPtr<IDXGISwapChain3> m_swapChain;
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    UINT m_rtvDescriptorSize;
+
+    //Sycronization objects
+    UINT m_frameIndex;
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_fenceValue;
+
+    void LoadPipeline();
+    void LoadAssets();
+    void PopulateCommandList();
+    void WaitForPreviousFrame();
+};
+/*
+1. Create the ID3D12Device using the D3D12CreateDevice function.
+2. Create an ID3D12Fence object and query descriptor sizes.
+3. Check 4X MSAA quality level support.
+4. Create the command queue, command list allocator, and main command list.
+5. Describe and create the swap chain.
+6. Create the descriptor heaps the application requires.(Types CBV/SRV/UAV, RTV, DSV)
+7. Resize the back buffer and create a render target view to the back buffer.
+8. Create the depth/stencil buffer and its associated depth/stencil view.
+9. Set the viewport and scissor rectangles.
+*/
